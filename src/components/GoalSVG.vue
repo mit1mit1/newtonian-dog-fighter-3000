fd74d77302ebae45b3e92c14ea1a15b7a35459e1
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2023-04-08 18:15:37 +1000

    tweak: radius of goal svgs

--- src/components/GoalSVG.vue
+++ src/components/GoalSVG.vue
@@ -18,9 +18,9 @@ export default defineComponent({
 </script>
 
 <template>
-    <circle v-if="shipState.ships.length > 0" class="goalSVG" :cx="positionX" :cy="positionY" :r="(radius ?? 0) + 3"
+    <circle v-if="shipState.ships.length > 0" class="goalSVG" :cx="positionX" :cy="positionY" :r="(radius ?? 0)"
         fill="transparent" stroke-width="4" :stroke="shipState.ships[0].nextGoal === index ? `url(#grad1)` : `green`" />
-    <circle v-if="shipState.ships.length > 1" class="goalSVG" :cx="positionX" :cy="positionY" :r="(radius ?? 0) - 4"
+    <circle v-if="shipState.ships.length > 1" class="goalSVG" :cx="positionX" :cy="positionY" :r="(radius ?? 0) - 6"
         fill="transparent" stroke-width="4" :stroke="shipState.ships[1].nextGoal === index ? `url(#grad2)` : `green`" />
     <text fill="white" stroke="white" class="goalText" :x="(positionX ?? 0) - 11" :y="(positionY ?? 0) + 13">{{ index
     }}</text>
